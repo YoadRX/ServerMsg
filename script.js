61b2dfd104236dc5e8090eb135b489aa1bcbd7b6
ori-eph <oriephrati@hilma.tech>
2023-10-26 10:35:42 +0300

    js changes

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const localUsers = localStorage.getItem('users');
 
-const holderUsers = {
-    createUser(){
-        const users = [];
-        if((localUsers === false)||(localUsers === null) || (localUsers.length === 0)){
-            const newUser = new this.buildUser("user1",1);
-            const newUser2 = new this.buildUser("user2",2);
-            users.push(newUser);
-            users.push(newUser2);
-            localStorage.setItem("users",JSON.stringify(users));
-        }
-    },
-    buildUser: function(name,id){
-        this.name = name
-        this.id = id
-    },
-}
-holderUsers.createUser();
-
 const server = {
     cables: [],
-    passToCable(userId, package) { },
     receivedPackages: [],
-    sendPackages() { },
+    passToCable(userId, package) {
+        for (let i = 0; i < this.cables.length; i++) {
+            if (this.cables[i]["id"] === userId) {
+                let sent = false;
+                const sendHelper = () => {
+                    if (!this.cables[i]["currentPack"]) {
+                        this.cables[i]["currentPack"] = package;
+                        this.cables[i].sendToUser();
+                        sent = true;
+                    }
+                }
+                while (!sent) {
+                    if (this.cables[i]["currentPack"]) {
+                        setTimeout( sendHelper, 3000);
+                    }
+                    else {
+                        sendHelper();
+                    }
+                }
+                return;
+            }
+        }
+    },
+    sendPackages() { 
+        for (let i=0; i<this.receivedPackages.length; i++) {
+            const cableId = this.receivedPackages[i]["receiver"];
+            this.passToCable(cableId, this.receivedPackages[i]);
+            this.receivedPackages.splice(i, 1);
+        }
+    }
 }
 
 class Cable {
@@ -31,23 +41,16 @@ class Cable {
         this.currentPack = false;
     }
     sendToUser() {
+        
     }
     sendToServer() {
         server.receivedPackages.push(this.currentPack);
     }
 }
 
-class package {
-    constructor(msg, origin, receiver) {
-        this.msg = msg;
-        this.origin = origin;
-        this.receiver = receiver;
-    }
-}
-
 class Client {
     constructor(user) {
-        
+
     }
     sendMsg() {
 
@@ -56,3 +59,31 @@ class Client {
 
     }
 }
+
+class MsgPackage {
+    constructor(msg, origin, receiver) {
+        this.msg = msg;
+        this.origin = origin;
+        this.receiver = receiver;
+    }
+}
+
+const holderUsers = {
+    createUser() {
+        const users = [];
+        if ((localUsers === false) || (localUsers === null) || (localUsers.length === 0)) {
+            const newUser = new this.buildUser("user1", 1);
+            const newUser2 = new this.buildUser("user2", 2);
+            users.push(newUser);
+            users.push(newUser2);
+            localStorage.setItem("users", JSON.stringify(users));
+        }
+    },
+    buildUser: function (name, id) {
+        this.name = name
+        this.id = id
+        server.cables.push(new Cable(id));
+    },
+}
+
+holderUsers.createUser();
