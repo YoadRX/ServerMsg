7e7d1ca428ee420d98db83ee0b2f0b216d32be72
ori-eph <oriephrati@hilma.tech>
2023-10-26 11:27:51 +0300

    not good

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const localUsers = localStorage.getItem('users');
 
 const server = {
+    users: localUsers,
     cables: [],
     receivedPackages: [],
     passToCable(userId, package) {
+        console.log(package + " is being passed to a cable");
         for (let i = 0; i < this.cables.length; i++) {
-            if (this.cables[i]["id"] === userId) {
+            if (this.cables[i]["id"] == userId) {
+                console.log("found the right user");
                 let sent = false;
                 const sendHelper = () => {
                     if (!this.cables[i]["currentPack"]) {
                         this.cables[i]["currentPack"] = package;
                         this.cables[i].sendToUser();
+                        console.log("passed from server");
                         sent = true;
                     }
                 }
                 while (!sent) {
                     if (this.cables[i]["currentPack"]) {
-                        setTimeout( sendHelper, 3000);
+                        setTimeout(sendHelper, 3000);
                     }
                     else {
                         sendHelper();
@@ -26,11 +30,12 @@ const server = {
             }
         }
     },
-    sendPackages() { 
-        for (let i=0; i<this.receivedPackages.length; i++) {
+    sendPackages() {
+        for (let i = 0; i < this.receivedPackages.length; i++) {
             const cableId = this.receivedPackages[i]["receiver"];
             this.passToCable(cableId, this.receivedPackages[i]);
             this.receivedPackages.splice(i, 1);
+            console.log("sending the packages");
         }
     }
 }
@@ -41,22 +46,29 @@ class Cable {
         this.currentPack = false;
     }
     sendToUser() {
-        
+        for (let i = 0; i < server.users.length; i++) {
+            if (server.users["id"] === this.id) {
+                server.users["client"].receiveMsg(this.currentPack);
+                this.currentPack = false;
+                console.log("passed to user client");
+                return;
+            }
+        }
     }
     sendToServer() {
         server.receivedPackages.push(this.currentPack);
+        server.sendPackages();
+        console.log("passed to server");
     }
 }
 
 class Client {
-    constructor(user) {
-
-    }
+    constructor() { }
     sendMsg() {
 
     }
-    receiveMsg() {
-
+    receiveMsg(msg) {
+        alert(msg);
     }
 }
 
@@ -77,11 +89,13 @@ const holderUsers = {
             users.push(newUser);
             users.push(newUser2);
             localStorage.setItem("users", JSON.stringify(users));
+            server.users = users;
         }
     },
     buildUser: function (name, id) {
         this.name = name
         this.id = id
+        this.Client = new Client();
         server.cables.push(new Cable(id));
     },
 }
